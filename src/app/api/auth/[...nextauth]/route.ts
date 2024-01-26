b3d56dc6a2e97fc30b57fa718ead19692b2dd7df
wuxiaoran <wuxiaoran@wjbrain.com>
2024-01-26 11:39:20 +0800

    feat: api routr

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,5 +3,5 @@ import NextAuth from "next-auth";
 import { authOptions } from "@/server/auth";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
